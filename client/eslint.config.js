b3b8ee73c129fd18b00f6db6cabbeabb95b5ef4a
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-29 17:04:52 -0300

    feat: update ESLint configuration and add simple-import-sort plugin

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,10 +1,11 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
@@ -20,6 +21,23 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+  },
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            ["^\\u0000"],
+            ["^react", "^@?\\w"],
+            ["^@/"],
+            ["^src/"],
+            ["^\\."],
+          ],
+        },
+      ],
     },
   },
 ]);
