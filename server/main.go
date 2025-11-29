f7af992901acf3bc456bed49781c3ce441767b73
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-29 17:20:48 -0300

    feat: implement HTTP server to serve static files and handle SPA routing

--- server/main.go
+++ server/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	distPath, err := filepath.Abs("../client/dist")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fs := http.FileServer(http.Dir(distPath))
+
+	serverPort := "8080"
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Try to serve static files first
+		path := filepath.Join(distPath, r.URL.Path)
+		if _, err := filepath.Glob(path); err == nil {
+			fs.ServeHTTP(w, r)
+			return
+		}
+
+		// fallback to index.html for SPA routing
+		http.ServeFile(w, r, filepath.Join(distPath, "index.html"))
+	})
+
+	log.Println("Server running at http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":"+serverPort, nil))
 }
