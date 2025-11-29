d709ba608326417aa45d4fac2446a2885c12492e
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-29 20:27:48 -0300

    refactor: simplify middleware chain creation by removing unnecessary variable

--- server/internal/middleware/chain.go
+++ server/internal/middleware/chain.go
@@ -7,8 +7,7 @@ type Middleware func(http.Handler) http.Handler
 func CreateChain(middlewares ...Middleware) Middleware {
 	return func(next http.Handler) http.Handler {
 		for i := len(middlewares) - 1; i >= 0; i-- {
-			x := middlewares[i]
-			next = x(next)
+			next = middlewares[i](next)
 		}
 
 		return next
