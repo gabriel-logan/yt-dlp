8f5ea86e316bf051ea14cb79bbc9787914737f07
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-29 22:18:06 -0300

    fix: remove unused PageExample component and its route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router";
 
 import HomePage from "./pages/Home";
 import NotFoundPage from "./pages/NotFound";
-import PageExample from "./pages/Page1";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/example" element={<PageExample />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
