223635ec55f6c8dda0441ba64c394865ce65579e
Gabriel Logan <gabrielcoutolima@protonmail.ch>
2025-11-29 22:27:07 -0300

    fix: reorder API key validation in AuthMiddleware for better readability

--- server/internal/middleware/auth.go
+++ server/internal/middleware/auth.go
@@ -3,16 +3,20 @@ package middleware
 import (
 	"net/http"
 	"os"
+	"strings"
 )
 
 func AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		apiKeyFromEnv := os.Getenv("X_API_KEY")
-		apiKeyFromHeader := r.Header.Get("X-API-KEY")
 
-		if apiKeyFromHeader != apiKeyFromEnv {
-			http.Error(w, "Unauthorized", http.StatusUnauthorized)
-			return
+		if strings.HasPrefix(r.URL.Path, "/api") {
+			apiKeyFromEnv := os.Getenv("X_API_KEY")
+			apiKeyFromHeader := r.Header.Get("X-API-KEY")
+
+			if apiKeyFromHeader != apiKeyFromEnv {
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
 		}
 
 		next.ServeHTTP(w, r)
